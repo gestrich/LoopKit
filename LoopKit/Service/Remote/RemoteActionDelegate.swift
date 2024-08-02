51eae50664b540345ca8bd837077c20c6c0b899e
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2024-08-02 09:14:30 -0400

    Extend remote command accepted period

--- LoopKit/Service/Remote/RemoteActionDelegate.swift
+++ LoopKit/Service/Remote/RemoteActionDelegate.swift
@@ -11,6 +11,6 @@ import Foundation
 public protocol RemoteActionDelegate: AnyObject {
     func enactRemoteOverride(name: String, durationTime: TimeInterval?, remoteAddress: String) async throws
     func cancelRemoteOverride() async throws
-    func deliverRemoteCarbs(amountInGrams: Double, absorptionTime: TimeInterval?, foodType: String?, startDate: Date?) async throws
-    func deliverRemoteBolus(amountInUnits: Double) async throws
+    func deliverRemoteCarbs(amountInGrams: Double, absorptionTime: TimeInterval?, foodType: String?, startDate: Date?, userCreatedDate: Date) async throws
+    func deliverRemoteBolus(amountInUnits: Double, userCreatedDate: Date) async throws
 }
